5709a57485f5b836c6a15e1dbb9066b6af47ebee
sarita jangid <sarita@gmail.com>
2022-10-15 11:58:19 +0530

    [Updated] Changes made in models.py file.

--- api/models.py
+++ api/models.py
@@ -11,11 +11,11 @@ GENDER_CHOICES = (
 class MCompany(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
-    # delete_status = models.BooleanField(blank=True, null=True)
-    # created_user_id = models.CharField(max_length=100)
-    # created_at = models.DateTimeField(blank=True, null=True)
-    # updated_user_id = models.CharField(max_length=100)
-    # updated_at = models.DateTimeField(blank=True, null=True)
+    delete_status = models.BooleanField(blank=True, null=True)
+    created_user_id = models.CharField(max_length=100)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_user_id = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -23,11 +23,11 @@ class MCompany(models.Model):
    
 class MDepartment(models.Model):
     name = models.CharField(max_length=100)    
-    # delete_status = models.BooleanField(blank=True, null=True)
-    # created_user_id = models.CharField(max_length=100)
-    # created_at = models.DateTimeField(blank=True, null=True)
-    # updated_user_id = models.CharField(max_length=100)
-    # updated_at = models.DateTimeField(blank=True, null=True)
+    delete_status = models.BooleanField(blank=True, null=True)
+    created_user_id = models.CharField(max_length=100)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_user_id = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -35,18 +35,17 @@ class MDepartment(models.Model):
 
 class TEmployee(models.Model):
     name = models.CharField(max_length=100)
-    age = models.IntegerField()
     address = models.CharField(max_length=100)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     department = models.ForeignKey(
         MDepartment, on_delete=models.CASCADE, null=True
     )
     company = models.ForeignKey(MCompany, on_delete=models.CASCADE, null=True)
-    # delete_status = models.BooleanField(blank=True, null=True)
-    # created_user_id = models.CharField(max_length=100)
-    # created_at = models.DateTimeField(blank=True, null=True)
-    # updated_user_id = models.CharField(max_length=100)
-    # updated_at = models.DateTimeField(blank=True, null=True)
+    delete_status = models.BooleanField(blank=True, null=True)
+    created_user_id = models.CharField(max_length=100)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_user_id = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
